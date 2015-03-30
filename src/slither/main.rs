d89f2f0df34d7416762d7cd354c7201086718a14
gifnksm <makoto.nksm+github@gmail.com>
2015-03-30 23:37:40 +0900

    update to rustc 1.0.0-nightly (c89de2c56 2015-03-28) (built 2015-03-29)

--- src/slither/main.rs
+++ src/slither/main.rs
@@ -5,7 +5,7 @@
 #![feature(unicode)]
 #![warn(bad_style,
         unused, unused_extern_crates, unused_import_braces,
-        unused_qualifications, unused_results, unused_typecasts)]
+        unused_qualifications, unused_results)]
 
 #![plugin(docopt_macros)]
 
