3763736e79969a14ea69f3ba97f8f503823228d3
gifnksm <makoto.nksm+github@gmail.com>
2015-08-16 22:39:25 +0900

    cli: small fixes.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,17 @@
+#![warn(bad_style)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+// #![warn(missing_docs)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unused)]
+#![warn(unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![warn(unused_qualifications)]
+#![warn(unused_results)]
+
 #![feature(libc)]
 #![feature(plugin)]
-#![warn(bad_style,
-        unused, unused_extern_crates, unused_import_braces,
-        unused_qualifications, unused_results)]
 
 #![plugin(docopt_macros)]
 
