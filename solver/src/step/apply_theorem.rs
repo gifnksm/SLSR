faee8bf21c09a88544e67bc0a317b633de205bf3
gifnksm <makoto.nksm+github@gmail.com>
2015-10-13 22:45:35 +0900

    solver: small refactoring

--- solver/src/step/apply_theorem.rs
+++ solver/src/step/apply_theorem.rs
@@ -8,7 +8,7 @@ use ::model::theorem::{Pattern, Theorem, TheoremMatcher};
 
 #[derive(Clone, Debug)]
 pub struct TheoremPool {
-    data: Vec<TheoremMatcher>
+    matchers: Vec<TheoremMatcher>
 }
 
 impl TheoremPool {
@@ -19,96 +19,119 @@ impl TheoremPool {
                       -> SolverResult<TheoremPool>
         where T: IntoIterator<Item=Theorem>
     {
-        let it = theo_defs
-            .into_iter()
-            .flat_map(|theo| theo.all_rotations());
+        let mut matchers = try!(create_matcher_list(theo_defs,
+                                                    puzzle,
+                                                    sum_of_hint,
+                                                    side_map));
 
-        let mut hint_theorem = [vec![], vec![], vec![], vec![], vec![]];
-        let mut nonhint_theorem = vec![];
-
-        for theo in it {
-            match theo.head() {
-                Pattern::Hint(h) => hint_theorem[h.hint() as usize].push(theo),
-                _ => nonhint_theorem.push(theo)
-            }
-        }
-
-        let mut data = vec![];
-
-        for p in puzzle.points() {
-            if let Some(x) = puzzle.hint(p) {
-                for theo in &hint_theorem[x as usize] {
-                    let o = match theo.head() {
-                        Pattern::Hint(hint) => hint.point(),
-                        _ => panic!()
-                    };
-                    let matcher = theo.clone().shift(p - o);
-                    try!(matcher.matches(puzzle, sum_of_hint, side_map))
-                        .update(side_map, &mut data);
-                }
-            }
-        }
-
-        for theo in nonhint_theorem {
-            let sz = theo.size();
-            for r in (1 - sz.0)..(puzzle.row() + sz.0 - 1) {
-                for c in (1 - sz.1)..(puzzle.column() + sz.1 - 1) {
-                    let matcher = theo.clone().shift(Move(r, c));
-                    try!(matcher.matches(puzzle, sum_of_hint, side_map))
-                        .update(side_map, &mut data);
-                }
-            }
-        }
-
-        let mut pool = TheoremPool { data: data };
-        let mut rev = side_map.revision();
         loop {
-            try!(pool.apply_all(side_map));
-            if side_map.revision() == rev {
-                break
+            let rev = side_map.revision();
+
+            try!(apply_all_theorem(&mut matchers, side_map));
+            if side_map.revision() != rev {
+                continue
             }
-            rev = side_map.revision();
+
+            break
         }
 
-        pool.merge_dup();
+        merge_duplicate_matchers(&mut matchers);
 
-        Ok(pool)
+        Ok(TheoremPool { matchers: matchers })
     }
 
     pub fn apply_all(&mut self, side_map: &mut SideMap) -> SolverResult<()> {
-        let cap = self.data.len();
+        apply_all_theorem(&mut self.matchers, side_map)
+    }
+}
 
-        for matcher in mem::replace(&mut self.data, Vec::with_capacity(cap)) {
-            try!(matcher.matches(side_map))
-                .update(side_map, &mut self.data);
+fn create_matcher_list<'a, T>(theo_defs: T,
+                              puzzle: &Puzzle,
+                              sum_of_hint: u32,
+                              side_map: &mut SideMap)
+                              -> SolverResult<Vec<TheoremMatcher>>
+    where T: IntoIterator<Item=Theorem>
+{
+    let it = theo_defs
+        .into_iter()
+        .flat_map(|theo| theo.all_rotations());
+
+    let mut hint_theorem = [vec![], vec![], vec![], vec![], vec![]];
+    let mut nonhint_theorem = vec![];
+
+    for theo in it {
+        match theo.head() {
+            Pattern::Hint(h) => hint_theorem[h.hint() as usize].push(theo),
+            _ => nonhint_theorem.push(theo)
         }
-
-        Ok(())
     }
 
-    fn merge_dup(&mut self) {
-        self.data.sort();
-        // Merge elements that have same matchers.
-        unsafe {
-            let len = self.data.len();
-            let p = self.data.as_mut_ptr();
+    let mut data = vec![];
 
-            let mut w = 1;
-            for r in 1..len {
-                let read = p.offset(r as isize);
-                let cmp = p.offset((w - 1) as isize);
+    for p in puzzle.points() {
+        if let Some(x) = puzzle.hint(p) {
+            for theo in &hint_theorem[x as usize] {
+                let o = match theo.head() {
+                    Pattern::Hint(hint) => hint.point(),
+                    _ => panic!()
+                };
+                let matcher = theo.clone().shift(p - o);
+                try!(matcher.matches(puzzle, sum_of_hint, side_map))
+                    .update(side_map, &mut data);
+            }
+        }
+    }
 
-                match (*cmp).merge(&*read) {
-                    Ok(()) => {}
-                    Err(()) => {
-                        let write = cmp.offset(1);
-                        mem::swap(&mut *write, &mut *read);
-                        w += 1;
-                    }
+    for theo in nonhint_theorem {
+        let sz = theo.size();
+        for r in (1 - sz.0)..(puzzle.row() + sz.0 - 1) {
+            for c in (1 - sz.1)..(puzzle.column() + sz.1 - 1) {
+                let matcher = theo.clone().shift(Move(r, c));
+                try!(matcher.matches(puzzle, sum_of_hint, side_map))
+                    .update(side_map, &mut data);
+            }
+        }
+    }
+
+    Ok(data)
+}
+
+fn apply_all_theorem(matchers: &mut Vec<TheoremMatcher>,
+                     side_map: &mut SideMap)
+                     -> SolverResult<()>
+{
+    let cap = matchers.len();
+
+    for m in mem::replace(matchers, Vec::with_capacity(cap)) {
+        try!(m.matches(side_map)).update(side_map, matchers);
+    }
+
+    Ok(())
+}
+
+fn merge_duplicate_matchers(matchers: &mut Vec<TheoremMatcher>) {
+    matchers.sort();
+
+    // Merge elements that have same matchers.
+    unsafe {
+        let len = matchers.len();
+        let p = matchers.as_mut_ptr();
+
+        let mut w = 1;
+        for r in 1..len {
+            let read = p.offset(r as isize);
+            let cmp = p.offset((w - 1) as isize);
+
+            match (*cmp).merge(&*read) {
+                Ok(()) => {}
+                Err(()) => {
+                    let write = cmp.offset(1);
+                    mem::swap(&mut *write, &mut *read);
+                    w += 1;
                 }
             }
-
-            self.data.truncate(w);
         }
+
+        matchers.truncate(w);
     }
 }
