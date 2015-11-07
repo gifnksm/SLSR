b9fe1631721507939095a3f05a212af0a861cc1f
gifnksm <makoto.nksm+github@gmail.com>
2015-11-07 23:53:19 +0900

    cli: extract solve module

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -16,65 +16,76 @@ extern crate slsr_core;
 extern crate slsr_solver;
 
 use std::{io, process};
-use std::fs::File;
 use std::io::prelude::*;
 
-use slsr_core::puzzle::Puzzle;
-use slsr_solver::{self as solver, Solutions};
-
 use error::AppResult;
-use parse_arg::{Config, OutputMode, SolveConfig};
+use parse_arg::Config;
 
 mod error;
 mod parse_arg;
 mod pprint;
 
-fn output(config: &SolveConfig, solution: Puzzle) -> io::Result<()> {
-    match config.output_mode {
-        OutputMode::Pretty(conf) => {
-            try!(pprint::print(&conf, &solution));
+mod solve {
+    use std::io;
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    use slsr_core::puzzle::Puzzle;
+    use slsr_solver::{self as solver, Solutions};
+
+    use error::AppResult;
+    use parse_arg::{OutputMode, SolveConfig};
+    use pprint;
+
+    pub fn run(config: SolveConfig) -> AppResult<()> {
+        if config.input_files.is_empty() {
+            try!(solve(&config, &mut io::stdin()));
+        } else {
+            for file in &config.input_files {
+                let mut f = try!(File::open(file));
+                try!(solve(&config, &mut f));
+            }
         }
-        OutputMode::Raw => {
-            print!("{}", solution.to_string());
-        }
-        OutputMode::None => {}
+
+        Ok(())
     }
 
-    Ok(())
-}
+    fn solve<T: Read>(config: &SolveConfig, input: &mut T) -> AppResult<()> {
+        let mut buf = String::new();
+        let _ = try!(input.read_to_string(&mut buf));
+        let puzzle = try!(buf.parse::<Puzzle>());
 
-fn solve<T: Read>(config: &SolveConfig, input: &mut T) -> AppResult<()> {
-    let mut buf = String::new();
-    let _ = try!(input.read_to_string(&mut buf));
-    let puzzle = try!(buf.parse::<Puzzle>());
-
-    if config.derive_all {
-        for solution in try!(Solutions::new(&puzzle)) {
+        if config.derive_all {
+            for solution in try!(Solutions::new(&puzzle)) {
+                try!(output(&config, solution));
+            }
+        } else {
+            let solution = try!(solver::solve(&puzzle));
             try!(output(&config, solution));
         }
-    } else {
-        let solution = try!(solver::solve(&puzzle));
-        try!(output(&config, solution));
+
+        Ok(())
     }
 
-    Ok(())
+    fn output(config: &SolveConfig, solution: Puzzle) -> io::Result<()> {
+        match config.output_mode {
+            OutputMode::Pretty(conf) => {
+                try!(pprint::print(&conf, &solution));
+            }
+            OutputMode::Raw => {
+                print!("{}", solution.to_string());
+            }
+            OutputMode::None => {}
+        }
+
+        Ok(())
+    }
 }
 
 fn run() -> AppResult<()> {
     match Config::parse() {
-        Config::Solve(config) => {
-            if config.input_files.is_empty() {
-                try!(solve(&config, &mut io::stdin()));
-            } else {
-                for file in &config.input_files {
-                    let mut f = try!(File::open(file));
-                    try!(solve(&config, &mut f));
-                }
-            }
-        }
+        Config::Solve(config) => solve::run(config),
     }
-
-    Ok(())
 }
 
 fn main() {
